7eb8c477b563104fee0996fc12004e69fab94577
arseni-baranov <arseni-baranov@yandex.ru>
2016-12-14 04:10:47 +0500

    Код переписан с нуля

--- github_trending.py
+++ github_trending.py
@@ -1,44 +1,59 @@
 import requests
 from datetime import date, timedelta
 
-
-def get_trending_repositories(time_period, amount):
-
-    payload = {
-        "q": "created:>{}".format(time_period),
-        "sort": "stars",
-        "order": "desc",
-        "per_page": amount
-    }
-
-    repositories_data = requests.get('https://api.github.com/search/repositories', payload).json()['items']
-    return [name for name in repositories_data]
+github_api = 'https://api.github.com'
+week_ago = date.today() - timedelta(days=7)
+top_size = 20
 
 
 def get_open_issues_amount(repo_name, repo_owner):
-    open_issues = requests.get('https://api.github.com/repos/{}/{}/issues'.format(repo_name, repo_owner)).json()
+    return len(requests.get('https://api.github.com/repos/{}/{}/issues'.format(repo_name, repo_owner)).json())
 
-    return len(open_issues)
+
+def format_json_data(json_data):
+    # uses get_open_issues_amount to count issues
+    repositories = []
+
+    for repo in json_data:
+        repository = {
+            'repo_name': repo['full_name'].split('/')[0],
+            'repo_owner': repo['full_name'].split('/')[1],
+            'stars': repo['stargazers_count'],
+            'url': repo['html_url'],
+            'issues': get_open_issues_amount(repo['full_name'].split('/')[0],
+                                             repo['full_name'].split('/')[1])
+        }
+
+        repositories.append(repository)
+
+    return repositories
+
+
+def get_trending_repositories(top_size):
+    params = {
+        "q": "created:>{}".format(week_ago),
+        "sort": "stars",
+        "order": "desc",
+        "per_page": top_size
+    }
+
+    return requests.get(github_api+'/search/repositories', params=params).json()['items']
+
+
+def pretty_print(repositories):
+    print(''.center(60, '='), end='\n')
+    print('Currently trending on github'.center(60, ' '), end='\n')
+    print(''.center(60, '='), end='\n\n')
+
+    for repo in repositories:
+        print('\nUrl: {}\nProject: {}\nAuthor: {}\nStars: {}\nIssues: {}\n'.format
+              (repo['url'], repo['repo_name'], repo['repo_owner'], repo['stars'], repo['issues']))
 
 
 def main():
-    week_ago = date.today() - timedelta(days=7)
-
-    print()
-    print('Most popular projects @ github:')
-    print()
-
-    try:
-        for name in get_trending_repositories(week_ago, 20):
-            repo_name, repo_owner = name['full_name'].split('/')
-
-            print('https://api.github.com/repos/{}/{}/issues'.format(repo_name, repo_owner))
-            print(repo_name, 'by', repo_owner)
-            print('stars: ', name['stargazers_count'])
-            print('issues:', get_open_issues_amount(repo_name, repo_owner))
-            print()
-    except Exception as error:
-        print('An error has occurred:', error)
+    json_data = get_trending_repositories(top_size)
+    repositories = format_json_data(json_data)
+    pretty_print(repositories)
 
 if __name__ == '__main__':
     main()
